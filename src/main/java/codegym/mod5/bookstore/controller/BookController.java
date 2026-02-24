0983cf025c23b5f3d165d81e23561799dd63ec07
Ivan Uresti <ivan.uresti@gmail.com>
2026-02-23 21:07:25 -0600

    Add spring-mvc

--- src/main/java/codegym/mod5/bookstore/controller/BookController.java
+++ src/main/java/codegym/mod5/bookstore/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/books")
-@Controller
+@RestController
 public class BookController {
 
     private final BookService bookService;
